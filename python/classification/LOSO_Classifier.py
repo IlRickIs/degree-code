b51017788a5d854d1001d7909dbdd328d59f86c9
Riccardo Mattia <riccardo.mattia2002@gmail.com>
2024-09-03 13:42:39 +0200

    refactored long method in LOSO_Classifier

--- python/classification/LOSO_Classifier.py
+++ python/classification/LOSO_Classifier.py
@@ -4,12 +4,10 @@ from sklearn.pipeline import make_pipeline
 from classification import helper
 from sklearn.preprocessing import StandardScaler
 import costants as C
-from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import LeaveOneGroupOut
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import os
 import pandas as pd
 import numpy as np
+from classification.MetricsHandler import MetricsHandler
 class LOSO_Classifier:
     def __init__(self, features, target, dataset_name):
         self.groups = features['actor']
@@ -17,42 +15,7 @@ class LOSO_Classifier:
         self.target = target
         self.dataset_name = dataset_name
 
-    def __big_report(self, actors, accuracies, precisions, 
-                     recalls, f1_scores, never_predicted,
-                     cumulative_cm, filename):
-        """Create a report for each actor, and the average metrics"""
-        os.makedirs(C.REPORTS_LOSO_PATH, exist_ok=True)
-        with open(filename, 'w') as f:
-            for i in range(len(actors)):
-                f.write(f'Actor: {actors[i]}\n')
-                f.write(f'Accuracy: {accuracies[i]}\n')
-                f.write(f'Precision: {precisions[i]}\n')
-                f.write(f'Recall: {recalls[i]}\n')
-                f.write(f'F1: {f1_scores[i]}\n')
-                f.write(f'Never predicted labels for this actor: {never_predicted[i]}\n\n')
-            
-            #remove all set() from never_predicted
-            never_predicted = [item for sublist in never_predicted for item in sublist]
-            #write the average metrics
-            print(f'\n---| Average metrics: |---')
-            report = f'Average accuracy: {sum(accuracies)/len(accuracies)}\n\
-                    Average precision: {sum(precisions)/len(precisions)}\n\
-                    Average recall: {sum(recalls)/len(recalls)}\n\
-                    Average F1: {sum(f1_scores)/len(f1_scores)}\n\
-                    never predicted labels: {never_predicted}\n'
-            print(report)
-            f.write(report)
-
-            #write the confusion matrix
-            if(C.NORMALIZE_MATRIX == 'true'):
-                row_sums = cumulative_cm.sum(axis=1, keepdims=True)
-                cumulative_cm = cumulative_cm / row_sums
-
-            helper.write_cool_confusion_matrix(cumulative_cm, 
-                                               ['neu', 'happy', 'sad', 'ang'], 
-                                               self.dataset_name, 'LOSO_svm')
-
-    def svm_classifier(self): #TODO: sometimes an actor get 1 in all the metrics, is not possible, correct this
+    def svm_classifier(self): 
         """Classify using SVM"""
         from sklearn.svm import SVC
 
@@ -63,17 +26,9 @@ class LOSO_Classifier:
         features = scaler.fit_transform(self.features)
         features = pd.DataFrame(features, columns=columns)
         
-        #inizializza le liste per le metriche
-        actors = []
-        accuracies = []
-        precisions = []
-        recalls = []
-        f1_scores = []
-        never_predicted_labels_list = []
-
-        #inizializza la matrice di confusione
+        #inizializza le liste per le metriche #TODO: refactor this by creating one object that contains all the metrics
         n_classes = len(np.unique(self.target))
-        cumulative_cm = np.zeros((n_classes, n_classes), dtype=int)
+        metrics_handler = MetricsHandler(self.dataset_name, 'LOSO_svm', n_classes)
 
         for train_idx, test_idx in loso.split(features, self.target, groups=self.groups):
             X_train, X_test = features.iloc[train_idx], features.iloc[test_idx]
@@ -83,32 +38,9 @@ class LOSO_Classifier:
             helper.optimize_svm_params(X_train, y_train, clf, self.dataset_name, C.PARAMS_LOSO_PATH)
             clf.fit(X_train, y_train)
             y_pred = clf.predict(X_test)
-
-            
-            # Calcola le metriche macro per multi-classe
-            never_predicted_labels = set(y_test) - set(y_pred)
-            precision = precision_score(y_test, y_pred, average='macro', zero_division=0)
-            accuracy = accuracy_score(y_test, y_pred)
-            recall = recall_score(y_test, y_pred, average='macro', zero_division=0)
-            f1 = f1_score(y_test, y_pred, average='macro', zero_division=0)
             actor = self.groups.iloc[test_idx[0]]
             
-            actors.append(actor)
-            precisions.append(precision)
-            accuracies.append(accuracy)
-            recalls.append(recall)
-            f1_scores.append(f1)
-            never_predicted_labels_list.append(never_predicted_labels)
-
-            # Calcola la matrice di confusione
-            cm = confusion_matrix(y_test, y_pred)
-            cumulative_cm += cm
+            #aggiungi le metriche al report
+            metrics_handler.add_actor_metrics(y_test, y_pred, actor)
         
-        self.__big_report(actors, 
-                          accuracies, 
-                          precisions, 
-                          recalls, 
-                          f1_scores,
-                          never_predicted_labels_list,
-                          cumulative_cm,
-                          C.REPORTS_LOSO_PATH + self.dataset_name + '_svm_report.txt')
+        metrics_handler.print_big_report()
---
+++ python/classification/MetricsHandler.py
@@ -0,0 +1,69 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import numpy as np
+import os
+import costants as C
+from classification import helper
+class MetricsHandler:
+    def __init__(self, dataset_name, classif_name, n_classes):
+        """Initialize the metrics handler\n
+        filename: str, the name of the file to write the metrics to,
+        for convention use the format: dataset_name_classifier_name"""
+        self.filename = C.REPORTS_LOSO_PATH + dataset_name + '_' + classif_name + '_report.txt'
+        self.dataset_name = dataset_name
+        self.classifier_name = classif_name
+        self.actors = []
+        self.accuracies = []
+        self.precisions = []
+        self.recalls = []
+        self.f1_scores = []
+        self.never_predicted = []
+        self.cumulative_cm = np.zeros((n_classes, n_classes), dtype=int)
+    
+    def add_actor_metrics(self, y_true, y_pred, actor):
+        """Calculate metrics for each actor""" 
+        self.actors.append(actor)
+        self.accuracies.append(accuracy_score(y_true, y_pred))
+        self.precisions.append(precision_score(y_true, y_pred, average='macro', zero_division=0))
+        self.recalls.append(recall_score(y_true, y_pred, average='macro', zero_division=0))
+        self.f1_scores.append(f1_score(y_true, y_pred, average='macro', zero_division=0))
+        self.never_predicted.append(list(set(y_true) - set(y_pred)))
+        self.cumulative_cm += confusion_matrix(y_true, y_pred)
+    
+    def get_metrics(self):
+        """Return metrics as a dictionary"""
+        return {'actors': self.actors,
+                'accuracies': self.accuracies,
+                'precisions': self.precisions,
+                'recalls': self.recalls,
+                'f1_scores': self.f1_scores,
+                'never_predicted': self.never_predicted,
+                'cumulative_cm': self.cumulative_cm}
+    
+    def print_big_report(self):
+        """Create a report for each actor, and the average metrics"""
+        with open(self.filename, 'w') as f:
+            for i in range(len(self.actors)):
+                f.write(f'Actor: {self.actors[i]}\n')
+                f.write(f'Accuracy: {self.accuracies[i]}\n')
+                f.write(f'Precision: {self.precisions[i]}\n')
+                f.write(f'Recall: {self.recalls[i]}\n')
+                f.write(f'F1: {self.f1_scores[i]}\n')
+                f.write(f'Never predicted labels for this actor: {self.never_predicted[i]}\n\n')
+
+            print(f'\n---| Average metrics: |---')
+            report = f'Average accuracy: {np.mean(self.accuracies)}\n\
+                    Average precision: {np.mean(self.precisions)}\n\
+                    Average recall: {np.mean(self.recalls)}\n\
+                    Average F1: {np.mean(self.f1_scores)}\n\
+                    never predicted labels: {list(set([item for sublist in self.never_predicted for item in sublist]))}\n'
+            print(report)
+            f.write(report)
+            
+        if(C.NORMALIZE_MATRIX == 'true'):
+            row_sums = self.cumulative_cm.sum(axis=1, keepdims=True)
+            self.cumulative_cm = self.cumulative_cm / row_sums
+
+        helper.write_cool_confusion_matrix(self.cumulative_cm, 
+                                            ['neu', 'happy', 'sad', 'ang'], 
+                                            self.dataset_name, self.classifier_name)
+        
