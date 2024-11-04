52c0cf16dea40f126674f096651ba694368883e1
Riccardo Mattia <riccardo.mattia2002@gmail.com>
2024-11-04 18:26:16 +0100

    started neural networks

--- python/deep_learning_classification.py/base_kfold_nn.py
+++ python/deep_learning_classification.py/base_kfold_nn.py
@@ -1,3 +1,9 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import tensorflow as tf
+
+print(tf.config.list_physical_devices())
